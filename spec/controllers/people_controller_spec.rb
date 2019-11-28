753a4d75404b912d8f0f4b9c72b511ead5d0db9f
Donnadieu <donnadieu.86@gmail.com>
2019-11-27 20:19:21 -0500

    Edited Person controller specs

--- spec/controllers/people_controller_spec.rb
+++ spec/controllers/people_controller_spec.rb
@@ -97,14 +97,16 @@ RSpec.describe PeopleController, type: :controller do
   describe "PUT #update" do
     context "with valid params" do
       let(:new_attributes) {
-        skip("Add a hash of attributes valid for your model")
+        { first_name: 'NewFirstName', last_name: 'NewLastName' }
       }
 
       it "updates the requested person" do
         person = Person.create! valid_attributes
-        put :update, params: {id: person.to_param, person: new_attributes}, session: valid_session
+        put :update, params: { id: person.to_param, person: new_attributes }, session: valid_session
         person.reload
-        skip("Add assertions for updated state")
+
+        expect(person.first_name).to eq('NewFirstName')
+        expect(person.last_name).to eq('NewLastName')
       end
 
       it "redirects to the person" do
