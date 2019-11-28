c34bb9baadf2537b93f3c311e48dde474cc3f645
Donnadieu <donnadieu.86@gmail.com>
2019-11-27 20:15:45 -0500

    Added specs for Person model

--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Person, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is invalid without a first_name' do
+    person = Person.new(first_name: nil)
+    expect(person).not_to be_valid
+  end
+
+  it 'is invalid without a last name' do
+    person = Person.new(first_name: 'Bob', last_name: nil)
+    expect(person).not_to be_valid
+  end
 end
